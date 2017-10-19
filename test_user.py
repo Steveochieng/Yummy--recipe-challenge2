83e43dff38635e143788bc569414efbe0020307e
Stephen Ochieng <31154917+Steveochieng@users.noreply.github.com>
2017-10-19 13:13:38 +0300

    Update test_user.py

--- test_user.py
+++ test_user.py
@@ -8,37 +8,37 @@ class  UserTest(unittest.TestCase):
 
         def test_user_registration(self):
             """This tests for complete fields"""
-            result=self.user.get_user_register("martin","akula","martin.akula@gmail.com","akulamartin","eat")
+            result=self.user.get_user_register("Stephen"," Ochieng","stephenochieng955@mail.com","stephenochieng","eat")
             self.assertEqual(1,result,"User registration successful")
 
         def test_empty_first_name_field(self):
             """Test for either empty field """
-            result=self.user.get_user_register("","akula","martin.akula@gmail.com","akulamartin","eat")
+            result=self.user.get_user_register("","Stephen"," Ochieng","stephenochieng955@mail.com","stephenochieng","eat")
             self.assertEqual(2,result,"Please fill in the first name field")
 
         def test_empty_second_name_field(self):
             """Test for either empty field """
-            result=self.user.get_user_register("martin","","martin.akula@gmail.com","akulamartin","eat")
+            result=self.user.get_user_register("Stephen"," Ochieng","stephenochieng955@mail.com","stephenochieng","eat")
             self.assertEqual(2,result,"Fill in the second name field please")
 
         def test_empty_email_field(self):
             """Test for either empty field """
-            result=self.user.get_user_register("martin","akula","","akulamartin","eat")
+            result=self.user.get_user_register("Stephen"," Ochieng","stephenochieng955@mail.com","stephenochieng","eat")
             self.assertEqual(2,result,"Fill in the email field please")
 
         def test_empty_user_name_field(self):
             """Test for either empty field """
-            result=self.user.get_user_register("martin","akula","martin.akula@gmail.com","","eat")
+            result=self.user.get_user_register("Stephen"," Ochieng","stephenochieng955@mail.com","stephenochieng","eat")
             self.assertEqual(2,result,"Fill in the username field please")
 
         def test_empty_password_field(self):
             """Test for either empty field """
-            result=self.user.get_user_register("martin","akula","martin.akula@gmail.com","akulamartin","")
+            result=self.user.get_user_register("Stephen"," Ochieng","stephenochieng955@mail.com","stephenochieng","eat"")
             self.assertEqual(2,result,"Fill in the password field please")
 
        # def test_user_in_register(self):
         #    """Test username and password"""
-         ##  result=self.user.get_user_register("martin","akula","martin.akula@gmail.com","akulamartin","eat")
+         ##  result=self.user.get_user_register("Stephen"," Ochieng","stephenochieng955@mail.com","stephenochieng","eat")
            # self.assertEqual(3,result,"Fill in the password field please")            
            
 if __name__=='__main__':
